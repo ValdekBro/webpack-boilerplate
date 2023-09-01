0291fc3aa10d57986f2de86a10fdfe3507c84ab3
Yaroslav Badyora <yaroslav.badyora@growthkitchen.co>
2023-09-01 21:20:36 +0300

    Setup

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.ts',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({  
+            template: __dirname + '/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        }),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    optimization: {
+        runtimeChunk: 'single',
+    },
+};
